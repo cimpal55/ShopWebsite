9f556c05da8ce218b185cb528cca2ac60be4cf96
cimpal55 <cimpals71@gmail.com>
2023-04-24 13:43:54 +0300

    Added product variants choice, discount prices and original prices, fixed design

---
+++ ShopWebsite/src/Server/Migrations/20230424101644_ProductVariants.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopWebsite.Server.Migrations
+{
+    public partial class ProductVariants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductVariants",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OriginalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductVariants", x => new { x.ProductId, x.ProductTypeId });
+                    table.ForeignKey(
+                        name: "FK_ProductVariants_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductVariants_ProductTypes_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Default" },
+                    { 2, "Paperback" },
+                    { 3, "E-Book" },
+                    { 4, "Audiobook" },
+                    { 5, "Stream" },
+                    { 6, "Blu-ray" },
+                    { 7, "VHS" },
+                    { 8, "PC" },
+                    { 9, "PlayStation" },
+                    { 10, "Xbox" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8112));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8140));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8143));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8145));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8146));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8148));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8150));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8151));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8153));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8155));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 24, 13, 16, 43, 811, DateTimeKind.Local).AddTicks(8156));
+
+            migrationBuilder.InsertData(
+                table: "ProductVariants",
+                columns: new[] { "ProductId", "ProductTypeId", "OriginalPrice", "Price" },
+                values: new object[,]
+                {
+                    { 1, 2, 19.99m, 9.99m },
+                    { 1, 3, 0m, 7.99m },
+                    { 1, 4, 29.99m, 19.99m },
+                    { 2, 2, 14.99m, 7.99m },
+                    { 3, 2, 0m, 6.99m },
+                    { 4, 5, 0m, 3.99m },
+                    { 4, 6, 0m, 9.99m },
+                    { 4, 7, 0m, 19.99m },
+                    { 5, 5, 0m, 3.99m },
+                    { 6, 5, 0m, 2.99m },
+                    { 7, 8, 29.99m, 19.99m },
+                    { 7, 9, 0m, 69.99m },
+                    { 7, 10, 59.99m, 49.99m },
+                    { 8, 8, 24.99m, 9.99m },
+                    { 9, 8, 0m, 14.99m },
+                    { 10, 1, 299m, 159.99m },
+                    { 11, 1, 399m, 79.99m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductVariants_ProductTypeId",
+                table: "ProductVariants",
+                column: "ProductTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductVariants");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(333));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(359));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(361));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(363));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(365));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(366));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(368));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(370));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(371));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(373));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "DateCreated",
+                value: new DateTime(2023, 4, 23, 23, 12, 25, 232, DateTimeKind.Local).AddTicks(375));
+        }
+    }
+}
--- ShopWebsite/src/Shared/Models/Data/Product.cs
+++ ShopWebsite/src/Shared/Models/Data/Product.cs
@@ -8,11 +8,9 @@ namespace ShopWebsite.Shared.Models.Data
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = "https://via.placeholder.com/300x300";
-        
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }
         public Category? Category { get; set; }
-        public DateTime DateCreated { get; set; } = DateTime.Now;
         public int CategoryId { get; set; }
+        public List<ProductVariant> Variants { get; set; } = new();
+        public DateTime DateCreated { get; set; } = DateTime.Now;
     }
 }
